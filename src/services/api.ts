3882504be646c1e1064be3d8edfe21b8f26173cf
ganssoftware <ganssoftware1@gmail.com>
2026-09-10 04:45:14 +0700

    vercel deploy commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL:
+        import.meta.env.VITE_API_URL ||
+        "https://be-simrs.vercel.app/api",
 
     headers: {
         "Content-Type": "application/json",
